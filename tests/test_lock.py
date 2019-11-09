ad983f5be28362a72fd4a977b43498d052f2639a
Aaron Bach <bachya1208@gmail.com>
2019-11-08 19:19:46 -0700

    Add test to ensure lock doesn't change state on error

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -64,6 +64,59 @@ async def test_lock_unlock(
 
 
 @pytest.mark.asyncio
+async def test_no_state_change_on_failure(aresponses, event_loop, v3_server):
+    """Test locking the lock."""
+    async with v3_server:
+        v3_server.add(
+            "api.simplisafe.com",
+            f"/v1/doorlock/{TEST_SUBSCRIPTION_ID}/{TEST_LOCK_ID}/state",
+            "post",
+            aresponses.Response(text=None, status=401),
+        )
+        v3_server.add(
+            "api.simplisafe.com",
+            "/v1/api/token",
+            "post",
+            aresponses.Response(text="", status=401),
+        )
+
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            api = await API.login_via_credentials(TEST_EMAIL, TEST_PASSWORD, websession)
+            systems = await api.get_systems()
+            system = systems[TEST_SYSTEM_ID]
+
+            lock = system.locks["987"]
+            assert lock.state == LockStates.locked
+
+            with pytest.raises(SimplipyError):
+                await lock.unlock()
+
+            assert lock.state == LockStates.locked
+
+
+@pytest.mark.asyncio
+async def test_properties(event_loop, v3_server):
+    """Test that lock properties are created properly."""
+    async with v3_server:
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            api = await API.login_via_credentials(TEST_EMAIL, TEST_PASSWORD, websession)
+            systems = await api.get_systems()
+            system = systems[TEST_SYSTEM_ID]
+
+            lock = system.locks["987"]
+            assert not lock.disabled
+            assert not lock.error
+            assert not lock.jammed
+            assert not lock.lock_low_battery
+            assert not lock.low_battery
+            assert not lock.offline
+            assert not lock.pin_pad_low_battery
+            assert not lock.pin_pad_offline
+
+            assert lock.state is LockStates.locked
+
+
+@pytest.mark.asyncio
 async def test_properties(event_loop, v3_server):
     """Test that lock properties are created properly."""
     async with v3_server:

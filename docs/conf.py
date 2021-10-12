bf719b4c2c13fe1da64d71c30be400c829723ea0
Aaron Bach <bachya1208@gmail.com>
2021-10-12 11:52:59 -0600

    Bump version to 12.0.0-alpha.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "12.0.0-alpha.4"
+release = "12.0.0-alpha.5"
 
 
 # -- General configuration ---------------------------------------------------

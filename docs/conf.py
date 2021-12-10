f57d1571a166ef9ca2fddac6a0a75c2fae14e4b1
Aaron Bach <bachya1208@gmail.com>
2021-12-09 20:35:50 -0700

    Bump version to 2021.12.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "2021.12.0"
+release = "2021.12.1"
 
 
 # -- General configuration ---------------------------------------------------

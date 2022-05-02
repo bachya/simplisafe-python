23b651d3893af104cb2c5719c6d7c16703d1d6a3
Aaron Bach <bachya1208@gmail.com>
2022-05-01 20:34:59 -0600

    Stuff we know works

--- examples/test_client_by_credentials.py
+++ examples/test_client_by_credentials.py
@@ -10,6 +10,8 @@ from simplipy.errors import SimplipyError, Verify2FAError, Verify2FAPending
 
 _LOGGER = logging.getLogger()
 
+DEFAULT_EMAIL_2FA_TIMEOUT = 500
+
 SIMPLISAFE_USERNAME = "<USERNAME>"
 SIMPLISAFE_PASSWORD = "<PASSWORD>"
 
@@ -33,12 +35,15 @@ async def main() -> None:
                 # loop and periodically see if the user has verified the email
                 # (eventually timing out if nothing happens):
                 try:
-                    async with timeout(30):
-                        try:
-                            await simplisafe.async_verify_2fa_email()
-                        except Verify2FAPending as err:
-                            _LOGGER.info(err)
-                            await asyncio.sleep(3)
+                    async with timeout(DEFAULT_EMAIL_2FA_TIMEOUT):
+                        while True:
+                            try:
+                                await simplisafe.async_verify_2fa_email()
+                            except Verify2FAPending as err:
+                                _LOGGER.info(err)
+                                await asyncio.sleep(3)
+                            else:
+                                break
                 except asyncio.TimeoutError as err:
                     raise Verify2FAError(
                         "Timed out while waiting for email-based 2FA verification"

07650ff09924404381f012f1620ccdb76e677be1
Aaron Bach <bachya1208@gmail.com>
2022-05-09 14:00:28 -0600

    Add myst-parser for Markdown based docs (#344)

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,12 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosectionlabel", "sphinx_rtd_theme"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

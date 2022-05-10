58f3e7caa0016e01411fdb1abde00f3dfb683d26
Aaron Bach <bachya1208@gmail.com>
2022-05-09 21:39:12 -0600

    Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -247,11 +247,18 @@ async def test_client_async_from_refresh_token(
 
 
 @pytest.mark.asyncio
-async def test_client_async_from_refresh_token_unknown_error():
+@pytest.mark.parametrize(
+    "raw_error,raised_error",
+    [
+        (aiohttp.client_exceptions.ClientResponseError, RequestError),
+        (Exception, SimplipyError),
+    ],
+)
+async def test_client_async_from_refresh_token_unknown_error(raised_error, raw_error):
     """Test an unknown error while creating a client from a refresh token."""
-    with patch("simplipy.API._async_api_request", AsyncMock(side_effect=Exception)):
+    with patch("simplipy.API._async_api_request", AsyncMock(side_effect=raw_error)):
         async with aiohttp.ClientSession() as session:
-            with pytest.raises(SimplipyError):
+            with pytest.raises(raised_error):
                 await API.async_from_refresh_token(TEST_REFRESH_TOKEN, session=session)
 
 

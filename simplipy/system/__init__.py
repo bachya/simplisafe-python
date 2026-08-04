ed3307db6c0cd9ab3786898d7dac54190ed68a2a
Aaron Graubert <aaron@graubert.com>
2026-08-04 11:08:11 -0400

    Adjust pin matching logic
    Current check against only reserved pins prevents updating any non-reserved pins when all 6 pins already set

--- simplipy/system/__init__.py
+++ simplipy/system/__init__.py
@@ -469,7 +469,7 @@ class System:  # pylint: disable=too-many-public-methods
             raise PinError(f"Refusing to create duplicate PIN: {pin}")
 
         max_pins = DEFAULT_MAX_USER_PINS + len(RESERVED_PIN_LABELS)
-        if len(latest_pins) == max_pins and label not in RESERVED_PIN_LABELS:
+        if len(latest_pins) == max_pins and label not in latest_pins:
             raise MaxUserPinsExceededError(
                 f"Refusing to create more than {max_pins} user PINs"
             )
